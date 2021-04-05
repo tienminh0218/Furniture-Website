d590b825b725e5362addd6c671d413e9fbad0311
Minh Tien <tienminh2018@gmail.com>
2021-04-05 16:17:54 +0700

    5/4/21- 4:17

--- src/public/js/modals.js
+++ src/public/js/modals.js
@@ -1,43 +1,51 @@
-var closeBtn = document.querySelectorAll('.close-modal');
-var clickOpenLoginModal = document.querySelector('.loginModal');
-var clickBgModal = document.querySelector('.modal-backgroundColor');
-var clickRegister = document.querySelector('.register-suggest');
+var closeBtn = document.querySelectorAll(".close-modal");
+var clickOpenLoginModal = document.querySelector(".loginModal");
+var clickBgModal = document.querySelector(".modal-backgroundColor");
+var registerSuggest = document.querySelector(".register-suggest a");
+var loginSuggest = document.querySelector(".login-suggest a");
 
-var mainModal = document.querySelector('.modals');
+var mainModal = document.querySelector(".modals");
 
-var modalContainers = document.querySelector('.modal-container');
-var modalRegisterForm = document.querySelector('.modal-registerForm');
-var modalLoginForm = document.querySelector('.modal-loginForm');
-
+var modalContainers = document.querySelector(".modal-container");
+var modalRegisterForm = document.querySelector(".modal-registerForm");
+var modalLoginForm = document.querySelector(".modal-loginForm");
 
 /// Button close modal form
-closeBtn.forEach(btnClose => {
-    btnClose.addEventListener('click', closeModal)
-})
+closeBtn.forEach((btnClose) => {
+    btnClose.addEventListener("click", closeModal);
+});
 
 /// Click background to close modal
-clickBgModal.addEventListener('click', closeModal)
+clickBgModal.addEventListener("click", closeModal);
 
 /// Click open modals
-clickOpenLoginModal.addEventListener('click', openModal)
+clickOpenLoginModal.addEventListener("click", openModal);
 
-/// Click open register form
-clickRegister.addEventListener('click', openRegister)
+/// Click change register form
+registerSuggest.addEventListener("click", ToggleForm);
 
+loginSuggest.addEventListener("click", ToggleForm);
 
+//// Toggle Form
+function ToggleForm() {
+    isLoginFormOpen = modalLoginForm.style.display;
 
-function openRegister () {
-    modalLoginForm.style.display = 'none';
-    modalRegisterForm.style.display = 'block';
+    if (isLoginFormOpen == "none") {
+        modalLoginForm.style.display = "block";
+        modalRegisterForm.style.display = "none";
+    } else {
+        modalLoginForm.style.display = "none";
+        modalRegisterForm.style.display = "block";
+    }
 }
 
 function closeModal() {
-    mainModal.style.display = 'none';
-    modalRegisterForm.style.display = 'none';
-    modalLoginForm.style.display = 'none';
+    mainModal.style.display = "none";
+    modalRegisterForm.style.display = "none";
+    modalLoginForm.style.display = "none";
 }
 
-function openModal () {
-    mainModal.style.display = 'block';
-    modalLoginForm.style.display = 'block';
+function openModal() {
+    mainModal.style.display = "block";
+    modalLoginForm.style.display = "block";
 }
