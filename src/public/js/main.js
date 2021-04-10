a81234d75e98032180b1aa5138c0028b37db4609
Minh Tien <tienminh2018@gmail.com>
2021-04-10 16:53:03 +0700

    login/register(85%)

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -74,22 +74,27 @@ function myFunction3() {
 var userExist = document.querySelector(".userExist");
 var containerUser = document.querySelector(".container-user");
 var logoutUser = document.querySelector(".logoutUser");
+var adminUser = document.querySelector(".adminUser");
 
 // check user logged
 if (userExist && containerUser) {
     userExist.onmouseover = toggleDialog;
     userExist.onmouseout = toggleDialog;
-
     logoutUser.addEventListener("click", eraseCookie);
+    adminUser.addEventListener(
+        "click",
+        () => (window.location.href = "http://localhost:3000/admin")
+    );
 }
 
-/// open dialog
+/// toggle dialog
 function toggleDialog(e) {
     containerUser.classList.toggle("showDisplay");
 }
 
 /// delete cookie
 function eraseCookie() {
+    console.log(logoutUser);
     document.cookie = "token" + "=; Max-Age=0";
     location.reload();
 }
