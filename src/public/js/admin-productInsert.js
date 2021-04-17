94a093be9423a9db68e51e8d2f71b47d77ff50d4
Minh Tien <tienminh2018@gmail.com>
2021-04-17 17:17:42 +0700

    preview image

---
+++ src/public/js/admin-productInsert.js
@@ -0,0 +1,13 @@
+var inputImage = document.querySelector(".form-control-typeFile");
+var imgPreview = document.querySelector(".preview-image img");
+
+inputImage.addEventListener("change", showPreview);
+
+/// Show image when upload
+function showPreview(event) {
+    if (event.target.files.length > 0) {
+        var src = URL.createObjectURL(event.target.files[0]);
+        imgPreview.src = src;
+        imgPreview.style.display = "block";
+    }
+}
