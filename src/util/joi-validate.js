dfca86e74d18b79d1c3251f2c2ee93768e61b07f
tranbuutai <tranbuutai80@gmail.com>
2021-04-18 12:01:56 +0700

    joi validate

--- src/util/joi-validate.js
+++ src/util/joi-validate.js
@@ -1,25 +1,90 @@
 const Joi = require("joi");
 
 /// Schema account login form
-var schemaLoginAccount = Joi.object({
-    username: Joi.string()
-        .error(() => "string message")
-        .alphanum()
-        .error(() => "alphanum message")
-        .min(4)
-        .error(() => "min message")
-        .max(30)
-        .error(() => "max message")
-        .required()
-        .error(() => "required message"),
+var schemaLoginAccount = Joi.object().keys({
+    username: Joi.string().alphanum().min(4).max(30).required(),
     password: Joi.string().alphanum().min(6).max(30).required(),
 });
 
 /// Schema account register form
-var schemaRegisterAccount = Joi.object({
-    username: Joi.string().alphanum().min(4).max(30).required(),
-    password: Joi.string().alphanum().min(6).max(30).required(),
-    fullname: Joi.string().min(6).max(50).required(),
+var schemaRegisterAccount = Joi.object().keys({
+    username: Joi.string()
+        .alphanum()
+        .min(4)
+        .max(30)
+        .required()
+        .error((errors) => {
+            errors.forEach((err) => {
+                switch (err.code) {
+                    case "string.empty":
+                    case "any.required":
+                        err.message = "Account should not be empty!";
+                        break;
+                    case "string.min":
+                        err.message = `Account should have at least ${err.local.limit} characters!`;
+                        break;
+                    case "string.max":
+                        err.message = `Account should have at most ${err.local.limit} characters!`;
+                        break;
+                    case "string.alphanum":
+                        err.message = "Đừng hack em anh ơi!!!!";
+                        break;
+                    default:
+                        break;
+                }
+            });
+            return errors;
+        }),
+    password: Joi.string()
+        .alphanum()
+        .min(6)
+        .max(30)
+        .required()
+        .error((errors) => {
+            errors.forEach((err) => {
+                switch (err.code) {
+                    case "string.empty":
+                    case "any.required":
+                        err.message = "Password should not be empty!";
+                        break;
+                    case "string.min":
+                        err.message = `Password should have at least ${err.local.limit} characters!`;
+                        break;
+                    case "string.min":
+                        err.message = `Password should have at most ${err.local.limit} characters!`;
+                        break;
+                    case "string.alphanum":
+                        err.message = "Đừng hack em anh ơi!!!!";
+                        break;
+                    default:
+                        break;
+                }
+            });
+            return errors;
+        }),
+    fullname: Joi.string()
+        .min(6)
+        .max(50)
+        .required()
+        .error((errors) => {
+            errors.forEach((err) => {
+                switch (err.code) {
+                    case "string.empty":
+                    case "any.required":
+                        err.message = "Full name should not be empty!";
+                        break;
+                    case "string.min":
+                        err.message = `Full name should have at least ${err.local.limit} characters!`;
+                        break;
+                    case "string.min":
+                        err.message = `Full name should have at most ${err.local.limit} characters!`;
+                        break;
+                    default:
+                        break;
+                }
+            });
+            return errors;
+        }),
     phonenumber: Joi.string()
         .pattern(/^[A-Z0-9-]/)
         .min(6)
