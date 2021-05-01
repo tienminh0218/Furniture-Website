9c19853392e0431a813042ce4eca969cf80d0c57
Minh Tien <tienminh2018@gmail.com>
2021-05-01 14:23:26 +0700

    cart

--- src/app/models/Cart.js
+++ src/app/models/Cart.js
@@ -3,12 +3,6 @@ const Schema = mongoose.Schema;
 const slug = require("mongoose-slug-generator");
 
 const Cart = new Schema({
-    // cartId: {
-    //     type: [Number],
-    //     default: function () {
-    //         return Math.random();
-    //     },
-    // },
     customer: {
         username: { type: String },
         fullname: { type: String },
@@ -21,17 +15,12 @@ const Cart = new Schema({
         {
             nameProduct: { type: String },
             priceProduct: { type: Number },
-            // descriptionProduct: { type: String },
-            // statusProduct: { type: String },
-            // inventoryProduct: { type: String },
             quantity: { type: string },
             imageProduct: { type: String },
-            // cloudinaryId_imageProduct: { type: String },
             nameCategory: { type: String },
             slug: { type: String },
         },
     ],
     totalPrice: { type: Number },
-    // slug: { type: String, slug: "cartId", unique: true },
 });
 module.exports = mongoose.model("cart", Cart);
