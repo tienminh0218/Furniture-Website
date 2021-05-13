237896c71973ca6d4a7e2d2fec4e2363d90ee0a5
Minh Tien <tienminh2018@gmail.com>
2021-05-13 18:40:20 +0700

    checkout

--- src/app/models/Invoice.js
+++ src/app/models/Invoice.js
@@ -1,28 +1,34 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Invoice = new Schema({
-    customer: {
-        username: { type: String },
-        fullname: { type: String },
-        emailaddress: { type: String },
-        phonenumber: { type: Number },
-        gender: { type: String },
-        address: { type: String },
-    },
-    products: [
-        {
-            nameProduct: { type: String },
-            priceProduct: { type: Number },
-            quantity: { type: Number },
-            imageProduct: { type: String },
-            nameCategory: { type: String },
-            slug: { type: String },
+const Invoice = new Schema(
+    {
+        customer: {
+            username: { type: String },
+            fullname: { type: String },
+            emailaddress: { type: String },
+            phonenumber: { type: Number },
+            gender: { type: String },
+            address: { type: String },
         },
-    ],
-    description: { type: String },
-    totalPrice: { type: Number },
-    totalQuantity: { type: Number },
-});
+        products: [
+            {
+                nameProduct: { type: String },
+                priceProduct: { type: Number },
+                quantity: { type: Number },
+                imageProduct: { type: String },
+                nameCategory: { type: String },
+                slug: { type: String },
+            },
+        ],
+        paymentOptions: { type: String },
+        description: { type: String },
+        totalPrice: { type: Number },
+        totalQuantity: { type: Number },
+    },
+    {
+        timestamps: true,
+    }
+);
 
 module.exports = mongoose.model("invoices", Invoice);
