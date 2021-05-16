1ee6adeb0c827aaf9218ab50af257447a0e81a7b
tranbuutai <tranbuutai80@gmail.com>
2021-05-16 16:49:24 +0700

    asd

--- src/public/js/previewImg.js
+++ src/public/js/previewImg.js
@@ -8,5 +8,7 @@ function showPreview(event) {
     if (event.target.files.length > 0) {
         var src = URL.createObjectURL(event.target.files[0]);
         imgPreview.src = src;
+        return;
     }
+    imgPreview.src = "";
 }
